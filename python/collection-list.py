1090eb9c0f5d1365bbcf5a4bdb029dc8b48a3861
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-05 17:18:50 +0200

    feat: lists or collections

---
+++ python/collection-list.py
@@ -0,0 +1,25 @@
+my_intergers = [1,2,3,4,5]
+
+print(my_intergers[0]) # 1
+print(my_intergers[1]) # 2
+print(my_intergers[3]) # 4
+
+relatives_names= [
+  "Toshiaki",
+  "Juliana",
+  "Yuji",
+  "Bruno",
+  "Kaio"
+  ]
+
+print(relatives_names[4]) # Kaio
+
+
+# append
+bookshelf = []
+
+bookshelf.append("The Effective Enginner")
+bookshelf.append("The 4 Four Work Week")
+
+print(bookshelf[0]) # The Effective Enginner
+print(bookshelf[1]) # The 4 Four Work Week
