a7dad167767d21fd822f05ef717dc772d2032831
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-05 16:59:27 +0200

    feat: learning conditional

---
+++ python/conditional.py
@@ -0,0 +1,15 @@
+
+if True: 
+  print("Hello Python If")
+
+if 2 > 1:
+  print('2 is greater than 1')
+
+#  elif
+
+if 1 > 2:
+  print('1 is greater than 2')
+elif 2 > 1:
+  print('1 is not greater than 2')
+else:
+  print('1 is equal to 2')
