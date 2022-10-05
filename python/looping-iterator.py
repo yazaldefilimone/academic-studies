4d03a72305bd07e7b4d2eb26b81588dbb9a0dc17
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-05 17:04:34 +0200

    feat: learning iterator and looping

---
+++ python/looping-iterator.py
@@ -0,0 +1,18 @@
+# Iterator
+num = 1
+
+while num <= 10:
+  print(num)
+  num += 1
+
+# simple
+loop_condition = True
+
+while loop_condition:
+  print("Loop Codition keeps: %s" %(loop_condition))
+  loop_condition = False
+
+# Loop
+
+for i in range(1, 11):
+  print(i)
